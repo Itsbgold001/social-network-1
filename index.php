63569c55695ab9e0fae8df6d6a808d90e2dce73d
siddharthkul <siddharthkul1995@gmail.com>
2017-07-08 23:13:13 +0530

    Added Post Class

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include('./classes/DB.php');
 include('./classes/Login.php');
+include('./classes/Post.php');
 $showTimeline = False;
 if (Login::isLoggedIn()) {
         $userid = Login::isLoggedIn();
@@ -16,7 +17,16 @@ WHERE posts.user_id = followers.user_id
 AND users.id = posts.user_id
 AND follower_id = :userid
 ORDER BY posts.likes DESC;',array(':userid'=>$userid));
-foreach ($followingposts as $post) {
-        echo $post['body']." ~ ".$post['username']."<hr />";
+foreach($followingposts as $post) {
+        echo $post['body']." ~ ".$post['username'];
+        echo "<form action='index.php?postid=".$post['id']."' method='post'>";
+        if (!DB::query('SELECT post_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$post['id'], ':userid'=>$userid))) {
+        echo "<input type='submit' name='like' value='Like'>";
+        } else {
+        echo "<input type='submit' name='unlike' value='Unlike'>";
+        }
+        echo "<span>".$post['likes']." likes</span>
+        </form>
+        <hr /></br />";
 }
 ?>
