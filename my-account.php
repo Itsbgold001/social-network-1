a1c20760c7311d35dac5c3111ce0da75c2cfaefd
siddharthkul <siddharthkul1995@gmail.com>
2017-07-10 15:27:21 +0530

    Fixed Image bug and added Image Posting Module

--- my-account.php
+++ my-account.php
@@ -7,22 +7,7 @@ if(Login::isLoggedIn()){
     die('Not Logged In');
 }
     if(isset($_POST['uploadprofileimg'])){
-        $image = base64_encode(file_get_contents($_FILES['profileimg']['tmp_name']));
-        $options = array('http'=>array(
-            'method'=>"POST",
-            'header'=>"Authorization: Bearer 9e0cd9c729a67e1b21ccd07f70ea7ad933f34aed\n".
-            "Content-Type: application/x-www-form-urlencoded",
-            'content'=>$image
-        ));
-        $context = stream_context_create($options);
-        $imgurURL = 'https://api.imgur.com/3/image';
-        if($_FILES['profileimg']['size'] > 10240000){
-            die('Image too big, must be less than 10MB');
-        }
-        //print_r($_FILES);
-        $response = file_get_contents($imgurURL, false, $context);
-        $response = json_decode($response);
-        DB::query('UPDATE users SET profileimg=:profileimg WHERE id=:userid',array(':profileimg'=>$response->data->link, ':userid'=>$userid));
+        Image::uploadImage('profileimg','UPDATE users SET profileimg=:profileimg WHERE id=:userid',array(':userid'=>$userid));
     }
 ?>
 
