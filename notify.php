d7e6c79c8d1a59257d91fb5c046fc01a610ce416
siddharthkul <siddharthkul1995@gmail.com>
2017-07-11 18:46:40 +0530

    Added receiving notifications module

--- notify.php
+++ notify.php
@@ -1,15 +1,25 @@
+
 <?php
 include('./classes/DB.php');
 include('./classes/Login.php');
 if (Login::isLoggedIn()) {
         $userid = Login::isLoggedIn();
 } else {
-        die('Not logged in');
+        echo 'Not logged in';
 }
-if(DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':userid'=>$userid))){
-    $notifications = DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':userid'=>$userid));
-    foreach($notifications as $n){
-        echo $n['type'];
-    } 
+echo "<h1>Notifcations</h1>";
+if (DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':userid'=>$userid))) {
+        $notifications = DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':userid'=>$userid));
+        foreach($notifications as $n) {
+                if ($n['type'] == 1) {
+                        $senderName = DB::query('SELECT username FROM users WHERE id=:senderid', array(':senderid'=>$n['sender']))[0]['username'];
+                        if ($n['extra'] == "") {
+                                echo "You got a notification!<hr />";
+                        } else {
+                                $extra = json_decode($n['extra']);
+                                echo $senderName." mentioned you in a post! - ".$extra->postbody."<hr />";
+                        }
+                }
+        }
 }
 ?>
